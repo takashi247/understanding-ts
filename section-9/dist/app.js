4742176332f0e71a37454f8351b5c11bc235f9ce
takashi247 <takashi247@gmail.com>
2022-10-22 22:33:33 +0900

    complete first lesson of section-9

--- section-9/dist/app.js
+++ section-9/dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Your code goes here...");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prjInputp = new ProjectInput();
+//# sourceMappingURL=app.js.map
