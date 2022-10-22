43581197aa7e0440bf367f5a8415e6346caadba1
takashi247 <takashi247@gmail.com>
2022-10-23 01:40:58 +0900

    complete section 14

--- section-14/src/App.tsx
+++ section-14/src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+
+const App: React.FC = () => {
+  const [id, setId] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    const newTodo = { id: "t" + id, text: text };
+    setId((prevId) => prevId + 1);
+    setTodos((prevTodo) => [...prevTodo, newTodo]);
+  };
+
+  const todoDeleteHandler = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onSubmit={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
 
 export default App;
